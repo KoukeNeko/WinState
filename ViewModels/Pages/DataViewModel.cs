4e03d474d134dd1253dd63bbc37a7d0c6c45d08c
江晉緯 <jjw@zentring.net>
2025-02-09 20:03:43 +0800

    修正無法尋覽至其他頁面的問題

--- ViewModels/Pages/DataViewModel.cs
+++ ViewModels/Pages/DataViewModel.cs
@@ -12,14 +12,6 @@ namespace WinState.ViewModels.Pages
         [ObservableProperty]
         private IEnumerable<DataColor> _colors;
 
-        public void OnNavigatedTo()
-        {
-            if (!_isInitialized)
-                InitializeViewModel();
-        }
-
-        public void OnNavigatedFrom() { }
-
         private void InitializeViewModel()
         {
             var random = new Random();
@@ -47,12 +39,19 @@ namespace WinState.ViewModels.Pages
 
         public Task OnNavigatedToAsync()
         {
-            throw new NotImplementedException();
+
+            if (!_isInitialized)
+            {
+                InitializeViewModel();
+            }
+            return Task.CompletedTask;
+            //throw new NotImplementedException();
         }
 
         public Task OnNavigatedFromAsync()
         {
-            throw new NotImplementedException();
+            return Task.CompletedTask;
+            //throw new NotImplementedException();
         }
     }
 }
