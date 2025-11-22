6a93f494d9ee695684267eb45bd59fccdff3dd5d
陳德生 <111033412+KoukeNeko@users.noreply.github.com>
2025-11-22 23:08:31 +0800

    fix: implement OnNavigatedFromAsync to return completed task instead of throwing exception

--- ViewModels/Pages/SettingsViewModel.cs
+++ ViewModels/Pages/SettingsViewModel.cs
@@ -66,7 +66,7 @@ namespace WinState.ViewModels.Pages
 
         public Task OnNavigatedFromAsync()
         {
-            throw new NotImplementedException();
+            return Task.CompletedTask;
         }
     }
 }
